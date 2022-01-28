c864a9a726ab5adbdb193bd05f458d9be51d8be7
samuel lutz <samlutz89@gmail.com>
2022-01-27 18:11:36 -0800

    pathing and routing

---
+++ routes/apiroutes.js
@@ -0,0 +1,33 @@
+// Dependencies
+const express = require("express").Router();
+const store = require("./../db/store")
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+// routes
+
+router.get("/notes", function (req, res) {
+    store
+        .getNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err))
+});
+
+router.post("/notes", function (req, res) {
+    store
+        .addNotes(req.body)
+        .then()
+        .catch(err => res.status(500).json(err))
+});
+
+router.delete("/notes", function (req, res) {
+    store
+        .deleteNotes()
+        .then()
+        .catch(err => res.status(500).json(err))
+});
+
---
+++ routes/htmlroutes.js
@@ -0,0 +1,20 @@
+// DEPENDENCIES
+
+const path = require("path");
+const router = require("express").Router();
+
+// routes
+
+
+    // HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
+    // ---------------------------------------------------------------------------
+
+    router.get("/notes", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
+    router.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
