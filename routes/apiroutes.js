c8e3502804f755de25d9b6ce3cd79381384e5b23
samuel lutz <samlutz89@gmail.com>
2022-01-30 08:29:11 -0800

    delete function working not removing all notes

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -15,8 +15,8 @@ router.post('/notes', (req, res) => {
     store.addNote(req.body)
     res.json(`${req.method} request received`);
     req.body.id = Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
-    store.push(req.body);
-    store.catch(err => res.status(500).json(err))
+    db.push(req.body);
+    // store.catch(err => res.status(500).json(err))
 });
 
 router.delete('/notes/:id', function (req, res) {
