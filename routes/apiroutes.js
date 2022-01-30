a50ae2728c0a9e007ed0c7cae187d39445c6c7f6
samuel lutz <samlutz89@gmail.com>
2022-01-30 08:21:38 -0800

    delete function working!

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,6 +2,7 @@
 const router = require("express").Router();
 const store = require("./../db/store");
 const fs = require('fs');
+var db = require("./../db/db.json");
 
 router.get("/notes", function (req, res) {
     store
@@ -18,10 +19,8 @@ router.post('/notes', (req, res) => {
     store.catch(err => res.status(500).json(err))
 });
 
-router.delete('/api/notes/:id', function (req, res) {
-  
-    // removes the note with matching id
-    store = store.filter(note => note.id !== req.params.id);
+router.delete('/notes/:id', function (req, res) {
+    db = db.filter(note => note.id !== req.params.id);
     fs.writeFile("./db/db.json", JSON.stringify(db, null, 2), (err) => err ? console.error(err) : console.log("success"));
     res.send(`Got a DELETE request at /user (${req.body})`)
   });
